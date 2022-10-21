4c0347fdec3162c251049e1e8d99d73e251ca4a0
Minsu <jminsu084@gmail.com>
2022-10-22 05:13:53 +0900

    Update : 숫자만 입력받고 4가지 연산 결과 모두 출력하도록 변경

--- ex1/dynamic.c
+++ ex1/dynamic.c
@@ -23,45 +23,78 @@ void main()
     char* cal = (char*)malloc(10*sizeof(char)); // 글자 수
     int num1, num2; // 입력받을 숫자
 
-    printf("무슨 연산을 할 것인가? (+, -, *, /) : ");
-    scanf("%s", cal);
+    // printf("무슨 연산을 할 것인가? (+, -, *, /) : ");
+    // scanf("%s", cal);
 
     printf("연산할 숫자 2개를 입력하시오 : ");
     scanf("%d %d", &num1, &num2);
 
-    if(strcmp(cal, "+") == 0) {
-        add = dlsym(handle, "add");
+    add = dlsym(handle, "add");
 
-        if((error = dlerror()) != NULL) {
-            fprintf(stderr, "%s", error);
-            exit(1);
-        }
-        printf("add(%d,%d) = %d\n", num1, num2, (*add) (num1,num2));
-    }else if(strcmp(cal, "-") == 0) {
-        subtract = dlsym(handle, "subtract");
+    if((error = dlerror()) != NULL) {
+        fprintf(stderr, "%s", error);
+        exit(1);
+    }
 
-        if((error = dlerror()) != NULL) {
-            fprintf(stderr, "%s", error);
-            exit(1);
-        }    
-        printf("subtract(%d,%d) = %d\n", num1, num2, (*subtract) (num1,num2));
-    }else if (strcmp(cal, "*") == 0) {
-        multiply = dlsym(handle, "multiply");
+    subtract = dlsym(handle, "subtract");
 
-        if((error = dlerror()) != NULL) {
-            fprintf(stderr, "%s", error);
-            exit(1);
-        }
-        printf("multiply(%d,%d) = %d\n", num1, num2, (*multiply) (num1,num2));
-    } else if (strcmp(cal, "/") == 0) {
-        divide = dlsym(handle, "divide");
+    if((error = dlerror()) != NULL) {
+        fprintf(stderr, "%s", error);
+        exit(1);
+    }    
 
-        if((error = dlerror()) != NULL) {
-            fprintf(stderr, "%s", error);
-            exit(1);
-        }
-        printf("divide(%d, %d) = %lf\n", num1, num2, divide(num1,num2));
-    }   
+    multiply = dlsym(handle, "multiply");
+
+    if((error = dlerror()) != NULL) {
+        fprintf(stderr, "%s", error);
+        exit(1);
+    }
+
+    divide = dlsym(handle, "divide");
+
+    if((error = dlerror()) != NULL) {
+        fprintf(stderr, "%s", error);
+        exit(1);
+    }
+
+    printf("add(%d,%d) = %d\n", num1, num2, (*add) (num1,num2));
+    printf("subtract(%d,%d) = %d\n", num1, num2, (*subtract) (num1,num2));
+    printf("multiply(%d,%d) = %d\n", num1, num2, (*multiply) (num1,num2));
+    printf("divide(%d, %d) = %lf\n", num1, num2, divide(num1,num2));
+
+    // if(strcmp(cal, "+") == 0) {
+    //     add = dlsym(handle, "add");
+
+    //     if((error = dlerror()) != NULL) {
+    //         fprintf(stderr, "%s", error);
+    //         exit(1);
+    //     }
+    //     printf("add(%d,%d) = %d\n", num1, num2, (*add) (num1,num2));
+    // }else if(strcmp(cal, "-") == 0) {
+    //     subtract = dlsym(handle, "subtract");
+
+    //     if((error = dlerror()) != NULL) {
+    //         fprintf(stderr, "%s", error);
+    //         exit(1);
+    //     }    
+    //     printf("subtract(%d,%d) = %d\n", num1, num2, (*subtract) (num1,num2));
+    // }else if (strcmp(cal, "*") == 0) {
+    //     multiply = dlsym(handle, "multiply");
+
+    //     if((error = dlerror()) != NULL) {
+    //         fprintf(stderr, "%s", error);
+    //         exit(1);
+    //     }
+    //     printf("multiply(%d,%d) = %d\n", num1, num2, (*multiply) (num1,num2));
+    // } else if (strcmp(cal, "/") == 0) {
+    //     divide = dlsym(handle, "divide");
+
+    //     if((error = dlerror()) != NULL) {
+    //         fprintf(stderr, "%s", error);
+    //         exit(1);
+    //     }
+    //     printf("divide(%d, %d) = %lf\n", num1, num2, divide(num1,num2));
+    // }   
     
 	dlclose(handle);
     
