5b75bea5c607ddaaa929a70285cd968176cad13a
minsu <jminsu084@gmail.com>
2022-10-22 02:54:43 +0900

    실습 예제 1번 사칙연산 정적 라이브러리 방식 풀이

---
+++ ex1/main.c
@@ -0,0 +1,11 @@
+/* main.c */
+#include <stdio.h>
+#include "staticlib.h"
+
+void main()
+{
+    printf("add(1, 2) = %d\n", add(1, 2));
+    printf("subtract(1, 2) = %d\n", subtract(1, 2));
+    printf("multiply(1, 2) = %d\n", multiply(1, 2));
+    printf("divide(2, 1) = %lf\n", divide(2, 1));
+}
